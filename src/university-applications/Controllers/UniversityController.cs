6ef7d8f6f2a75b6232443dd683993216fa6d588c
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2023-02-26 17:55:31 -0300

    :sparkles: Implementando controller com endpoints e busca por universidades do pais e busca por parte do nome e pais

--- src/university-applications/Controllers/UniversityController.cs
+++ src/university-applications/Controllers/UniversityController.cs
@@ -1,14 +1,48 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using university_applications.Services;
+
 
 namespace university_applications.Controllers
 {
   [ApiController]
   [Route("university")]
+
   public class UniversityController : ControllerBase, IUniversityController
   {
-    public UniversityController()
+    private readonly IUniversityService _universityService;
+
+    public UniversityController(IUniversityService universityService)
     {
-      throw new System.NotImplementedException();
+      _universityService = universityService;
     }
+
+    [HttpGet("Country&Name")]
+    public async Task<IActionResult> FindUniversity(string country, string name)
+    {
+      var university = await _universityService.FindUniversity(name, country);
+
+      if (university == null)
+      {
+        return NotFound();
+      }
+
+      return Ok(university);
+    }
+
+    [HttpGet("Country")]
+    public async Task<IActionResult> FindUniversity(string country)
+    {
+      var university = await _universityService.FindUniversity(country);
+
+      if (university == null)
+      {
+        return NotFound();
+      }
+
+      return Ok(university);
+    }
+
   }
 }
+
