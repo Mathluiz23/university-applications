b55fa6583807ab800f3ac626df559ea2672aa9c0
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2023-02-26 18:14:50 -0300

    Implementando testes de integração

--- src/university-applications.Test/UniversityIntegrationTest.cs
+++ src/university-applications.Test/UniversityIntegrationTest.cs
@@ -1,7 +1,15 @@
 namespace university_applications.Test;
+using System.Threading.Tasks;
+using Xunit;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
 
-public class UniversityIntegrationTest
+public class UniversityIntegrationTest : IClassFixture<TestingWebAppFactory<Program>>
 {
+  private readonly HttpClient _httpClient;
+
+  public UniversityIntegrationTest(TestingWebAppFactory<Program> factory) => _httpClient = factory.CreateClient();
 
   [Theory]
   [InlineData("Brazil", "federal")]
@@ -9,7 +17,13 @@ public class UniversityIntegrationTest
 
   public async Task ShouldFindAUniversityByCountryAndName(string country, string name)
   {
-    throw new System.NotImplementedException();
+    var response = await _httpClient.GetAsync($"/university/Country&Name/?name={name}&country={country}");
+    response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+    var result = await response.Content.ReadFromJsonAsync<object>();
+    result.Should().BeOfType<JsonElement>();
+    result?.ToString().Should().Contain(country);
+    result?.ToString()!.ToLower().Should().Contain(name);
   }
 
   [Theory]
@@ -17,6 +31,11 @@ public class UniversityIntegrationTest
   [InlineData("Turkey")]
   public async Task ShouldFindAUniversityByCountry(string country)
   {
-    throw new System.NotImplementedException();
+    var response = await _httpClient.GetAsync($"/university/Country/?country={country}");
+    response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+    var result = await response.Content.ReadFromJsonAsync<object>();
+    result.Should().BeOfType<JsonElement>();
+    result?.ToString().Should().Contain(country);
   }
 }
