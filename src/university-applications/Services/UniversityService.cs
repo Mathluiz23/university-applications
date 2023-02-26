1e15c36f00100f526ecc812c3d45c0de99a98114
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2023-02-26 18:07:16 -0300

    :sparkles: Implementando service com endpoints e serviço de requisição na url externa

--- src/university-applications/Services/UniversityService.cs
+++ src/university-applications/Services/UniversityService.cs
@@ -2,9 +2,36 @@ namespace university_applications.Services
 {
   public class UniversityService : IUniversityService
   {
-    public UniversityService()
+    private readonly HttpClient _httpClient;
+
+    private readonly string _remoteServiceBaseUrl = "http://universities.hipolabs.com";
+
+    public UniversityService(HttpClient client)
     {
-      throw new System.NotImplementedException();
+      _httpClient = client;
+      _httpClient.BaseAddress = new Uri(_remoteServiceBaseUrl);
+    }
+
+    public async Task<object> FindUniversity(string name, string country)
+    {
+      var response = await _httpClient.GetAsync($"/search?{name}&{country}");
+
+      response.EnsureSuccessStatusCode();
+
+      var result = await response.Content.ReadFromJsonAsync<object>();
+
+      return result;
+    }
+
+    public async Task<object> FindUniversity(string country)
+    {
+      var response = await _httpClient.GetAsync($"/search?{country}");
+
+      response.EnsureSuccessStatusCode();
+
+      var result = await response.Content.ReadFromJsonAsync<object>();
+
+      return result;
     }
   }
 }
